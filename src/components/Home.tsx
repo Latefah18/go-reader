462b2040546966ff3914a67c401caba07c1038f0
Latefah18 <LatefahBarnawi@gmail.com>
2022-08-10 03:31:36 +0300

    home-page-edit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,74 @@
 import { InputText } from "primereact/inputtext";
-import React from 'react';
+import React, { useRef } from 'react';
 import { Splitter, SplitterPanel } from 'primereact/splitter';
+
+import  { useState, useEffect } from 'react';
+import { OrderList } from 'primereact/orderlist';
+import { ProductService } from '../service/ProductService';
+import { Button } from "primereact/button";
 
 
 export function Home(){
 
     
+    const [filterValue, setFilterValue] = useState('');
+    const filterInputRef :any= useRef();
+    const [products, setProducts] = useState();
+    const productService = new ProductService();
 
 
 
+
+    const filterTemplate = (options:any) => {
+        let {filterOptions} = options;
+    
+        return (
+            <div className="flex">
+              <div className="flex m-2"> <InputText className="search-bar" value={filterValue} ref={filterInputRef} onChange={(e) => myFilterFunction(e, filterOptions)} /> </div> 
+              <div>   <Button className="cancel-btn" label=" cancel" onClick={() => myResetFunction(filterOptions)} /></div> 
+            </div>
+        )
+    }
+    
+    const myResetFunction = (options:any) => {
+        setFilterValue('');
+        options.reset();
+        filterInputRef && filterInputRef.current.focus()
+    }
+    
+    const myFilterFunction = (event:any, options:any) => {
+        let _filterValue = event.target.value;
+        setFilterValue(_filterValue);
+        options.filter(event);
+    }
+
+
+
+
+    useEffect(() => {
+        productService.getProductsSmall().then(data => setProducts(data));
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    const itemTemplate = (item:any) => {
+        return (
+            <div className="product-item">
+                <div className="image-container">
+                    <img src={`images/product/${item.image}`} onError={(e) => e.currentTarget.src='https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} alt={item.name} />
+                </div>
+                <div className="product-list-detail">
+                    <h5 className="mb-2">{item.name}</h5>
+                    <i className="pi pi-tag product-category-icon"></i>
+                    <span className="product-category">{item.category}</span>
+                </div>
+                <div className="product-list-action">
+                    <h6 className="mb-2">${item.price}</h6>
+                    <span className={`product-badge status-${item.inventoryStatus.toLowerCase()}`}>{item.inventoryStatus}</span>
+                </div>
+            </div>
+        );
+    }
 
     return(
-        
         
         <><div className="card">
             <h5 className="head"> Born to read go to read 🚶📚</h5>
@@ -42,10 +99,20 @@ export function Home(){
 
         </div><div className="flex flex-row flex-wrap card-container blue-containe p-8">
 
+<div>
 
-                <div>ggg</div>
+<div className="orderlist-demo">
+            <div className="card">
+           
+            <OrderList value={products} header="what do you want to read " dataKey="id" itemTemplate={itemTemplate} filter filterPlaceholder="search by name"  filterBy="name"  filterTemplate={filterTemplate} >    </OrderList>
+ 
 
-
+            </div>
+        </div>
+</div>
+                <div>
+                    
+                </div>
 
                 <div className="head">Best of 2022</div>
 
--- src/service/PhotoService.tsx
+++
@@ -1,9 +0,0 @@
-
-export class PhotoService {
-
-    getImages() {
-        return fetch('data/photos.json').then(res => res.json())
-                .then(d => d.data);
-    }
-}
-  
